816b397443e13efb515bca9a2792a04d9d2c7440
mhammadgammal <mhammadgamal17@gmail.com>
2025-10-25 23:34:56 +0300

    - add web starter dependency - add test and hello GET endpoints

--- src/main/java/com/jsbprojs/hello_world/demo/DemoApplication.java
+++ src/main/java/com/jsbprojs/hello_world/demo/DemoApplication.java
@@ -2,11 +2,26 @@ package com.jsbprojs.hello_world.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.err.println("http://localhost:8080/");
 	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index Page";
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+	
 }
