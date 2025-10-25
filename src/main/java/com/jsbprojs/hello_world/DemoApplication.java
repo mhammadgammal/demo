6ba488161fcc870da2d55921bc67fd27641152c0
mhammadgammal <mhammadgamal17@gmail.com>
2025-10-25 23:36:50 +0300

    mv file to parent dir

--- src/main/java/com/jsbprojs/hello_world/DemoApplication.java
+++ src/main/java/com/jsbprojs/hello_world/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.jsbprojs.hello_world.demo;
+package com.jsbprojs.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
